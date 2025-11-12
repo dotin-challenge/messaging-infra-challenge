ac7c0b7ec9d1f261679b2efbcca87ec40c2b4a49
Amin Saffarnejad <vira1368@gmail.com>
2025-11-12 16:42:59 +0330

    Add models.

---
+++ solutions/C#/saffarnejad/Common/Models/RabbitMQConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace Common.Models
+{
+    public class RabbitMQConfig
+    {
+        public string Host { get; set; } = "localhost";
+        public string User { get; set; } = "guest";
+        public string Password { get; set; } = "guest";
+        public int Port { get; set; } = 5672;
+        public string VirtualHost { get; set; } = "/";
+    }
+}
